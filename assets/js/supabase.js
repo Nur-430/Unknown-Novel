ad51ec3ce8b52fcdc3128d937e2958790dafb895
Nur-430 <nurokhim430@gmail.com>
2026-01-16 03:55:19 +0700

    mantap

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,11 +1,7 @@
-<!-- assets/js/supabase.js -->
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
-<script>
 const SUPABASE_URL = "https://nbdqvvhoqbahkfzwkppw.supabase.co";
 const SUPABASE_KEY = "sb_publishable_f_SXDmpiAE0l0qYthRvBCw_WJmvA43U";
 
-const supabase = supabase.createClient(
+const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
-</script>
