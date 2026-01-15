a4c88200362fda34ef7a1b0bc24768328962b8c5
Nur-430 <nurokhim430@gmail.com>
2026-01-16 04:13:55 +0700

    gg

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://nbdqvvhoqbahkfzwkppw.supabase.co";
 const SUPABASE_KEY = "sb_publishable_f_SXDmpiAE0l0qYthRvBCw_WJmvA43U";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
